7977ee01f6428fa0903806558db3870284f4a18d
Sandeep-KSanthosh <sandeepks2807@gmail.com>
2024-02-27 20:41:09 +0530

    login and signup name

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-blue-800'>
+			<h1 className='text-4xl font-bold text-center mb-5 text-blue-900' style={{ fontFamily: 'cursive' }}>
 					ChatterUp
 					{/* <span className='text-blue-500'> ChatApp</span> */}
 				</h1>
